55decdb526d590da3178ba5593a36a08c235aad0
unknown <chetanzagade28@gmail.com>
2024-04-21 21:30:48 +0530

    getter and setter

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello World");
+class user {
+    constructor(email) {
+        this.age = 0; //it need to initalise
+        this.city = "Delhi";
+        this.state = "Delhi";
+        this.email = email;
+    }
+    get giving() {
+        return `hello ${this.state}`;
+    }
+    set taking(state) {
+        this.state = state;
+    }
+}
+const a = new user("a@gmail.com");
+a.age = 4;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World"); 
+class user{
+    email:string
+    age:number = 0//it need to initalise
+    readonly city:string = "Delhi"
+    private state:string = "Delhi"
+    constructor(email:string){
+        this.email = email;
+    }
+    get giving():string{
+        return `hello ${this.state}`
+    }
+    set taking(state: string){
+        this.state =state;
+    }
+}
+
+const a = new user("a@gmail.com");
+a.age = 4
