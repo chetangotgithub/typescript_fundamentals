1f32c30ffaef13fae350521f94e8e526800c6b20
unknown <chetanzagade28@gmail.com>
2024-04-30 10:46:15 +0530

    UnionFind

--- dist/UnionFind/unionfond.ts
+++ dist/UnionFind/unionfond.ts
@@ -1,39 +1,50 @@
 class UFind {
-    a: number;
-    arr:number[] = [];
-    constructor (a: number){
-        this.a = a;
-        for(var i = 0; i < a ; i++){
-            this.arr[i] = i;
+    n: number;
+    parent:number[] = [];
+    rank:number[] = [];
+    constructor (n: number){
+        this.n = n;
+        for(var i = 0; i < n ; i++){
+            this.parent[i] = i;
+            this.rank[i] = 0;
         }
     }
-    find (num1: number , num2:number) : boolean{
-        return this.arr[num1] == this.arr[num2];
+    find (num1: number) : number{
+        if(num1 == this.parent[num1]){
+          return num1;
+        }
+        return this.parent[num1] = this.find(this.parent[num1]);
     }
 
     union (num1: number , num2:number) : void{
-        var id1 = this.arr[num1 - 1];
-        var id2 = this.arr[num2 - 1];
+        var id1 = this.parent[num1];
+        var id2 = this.parent[num2];
         if(id1 == id2){
             return;
         }
-        for(var i = 0; i < this.a ; i++){
-            if(this.arr[i] == id1){
-                this.arr[i] = id2;
-            }
+        if(this.rank[id1] > this.rank[id2]){
+          this.parent[id2] = id1;
+        }else if(this.rank[id1] < this.rank[id2]){
+          this.parent[id1] = id2;
+        }else {
+          this.parent[id1] = id2;
+          this.rank[id1] += 1;
         }
         //this.arr[num1 - 1] = id2;
     }
 }
 
 const Unionfind = new UFind(5);
+Unionfind.union(2 , 3);
+console.log("first union " , Unionfind.parent);
 Unionfind.union(3 , 4);
-console.log("first union " + Unionfind.arr);
-Unionfind.union(4 , 5);
-console.log("second union " + Unionfind.arr);
-Unionfind.union(1 , 5);
-console.log("third union " + Unionfind.arr);
-//console.log(unionfind.find(3 , 4));
-//[LOG]: "first union 0,1,3,3,4" 
-//[LOG]: "second union 0,1,4,4,4" 
-//[LOG]: "third union 4,1,4,4,4"
+console.log("second union " , Unionfind.parent);
+console.log("Parents of 2 and 4 are same : " , Unionfind.find(2) == Unionfind.find(4));
+Unionfind.union(1 , 4);
+console.log("third union " , Unionfind.parent);
+console.log("Parents of 1 and 4 are same : " , Unionfind.find(1) == Unionfind.find(4));
+// [LOG]: "first union ",  [0, 1, 3, 3, 4] 
+// [LOG]: "second union ",  [0, 1, 3, 4, 4] 
+// [LOG]: "Parents of 2 and 4 are same : ",  true 
+// [LOG]: "third union ",  [0, 4, 4, 4, 4] 
+// [LOG]: "Parents of 1 and 4 are same : ",  true 
